400a945d0ebd84412d6ce291701d9ddc70eb2b5e
Vibhor <vibhorgupta04@gmail.com>
2023-07-01 21:27:54 +0530

    refactoring

--- src/components/dashboard/Portfolio.tsx
+++ src/components/dashboard/Portfolio.tsx
@@ -7,27 +7,27 @@ ChartJS.register(ArcElement, Tooltip, Legend);
 
 const Portfolio = () => {
   const [user, setUser] = React.useState({
-    labels: UserData.map((data) => data.year),
+    // labels: UserData.map((data) => data.coin),
     datasets: [
       {
-        label: 'User Gained',
-        data: UserData.map((data) => data.userGain),
+        label: 'Value $',
+        data: UserData.map((data) => data.value),
         backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(255, 159, 64, 0.2)',
+          'rgba(255, 99, 132, 0.5)',
+          'rgba(153, 102, 255, 0.5)',
+          'rgba(255, 159, 64, 0.5)',
           'rgba(255, 205, 86, 0.2)',
           'rgba(75, 192, 192, 0.2)',
           'rgba(54, 162, 235, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
           'rgba(201, 203, 207, 0.2)',
         ],
         borderColor: [
           'rgb(255, 99, 132)',
+          'rgb(153, 102, 255)',
           'rgb(255, 159, 64)',
           'rgb(255, 205, 86)',
           'rgb(75, 192, 192)',
           'rgb(54, 162, 235)',
-          'rgb(153, 102, 255)',
           'rgb(201, 203, 207)',
         ],
         borderWidth: 1,
@@ -42,14 +42,23 @@ const Portfolio = () => {
           Total value <span className="text-black font-semibold">$1000</span>
         </div>
       </div>
-      <div className="flex items-center gap-4 mt-6">
+      <div className="flex flex-wrap-reverse lg:flex-nowrap justify-center  items-center gap-4 mt-6">
         <div className="">
           <Pie data={user} />
         </div>
-        <div>
-          <div>∙ BitCoin</div>
-          <div>∙ Ithereum</div>
-          <div>∙ Binaca</div>
+        <div className='flex flex-col gap-2 items-start text-sm'>
+          <div className='flex gap-2'>
+            <span className=' px-6 border-2 border-red-2 bg-red-3'></span> 
+            <span>Bitcoin</span>
+          </div>
+          <div className='flex gap-2'>
+            <span className=' px-6 border-2 border-blue-5 bg-blue-4'></span> 
+            <span>Ethereum</span>
+          </div>
+          <div className='flex gap-2'>
+            <span className='px-6 border-2 border-yellow-1 bg-yellow-2'></span> 
+            <span>Tether</span>
+          </div>
         </div>
       </div>
     </div>
--- src/components/dashboard/Search.tsx
+++ src/components/dashboard/Search.tsx
@@ -1,5 +1,4 @@
-import { useState } from 'react';
-
+import React, { useState, useEffect } from 'react';
 import { SearchLineIcon } from '../common/Icons';
 import { useSelector, useDispatch } from 'react-redux';
 import { coinValue } from '../../store';
@@ -9,135 +8,149 @@ const Search = () => {
   const dispatch = useDispatch();
   const [openSearch, setOpenSearch] = useState(false);
   const [currencyName, setCurrencyName] = useState('');
-
   const [loading, setLoading] = useState(false);
-
   const [currency, setCurrency] = useState('');
   const [searchResult, setSearchResult] = useState<any>(null);
-
-  console.log('searchResult>>>>', searchResult);
 
   const coinsTrending = useSelector((state: any) => {
     return state.api.trendCoin;
   });
 
-  console.log('coinsTrending >>>', coinsTrending);
+  useEffect(() => {
+    let timer: NodeJS.Timeout;
 
-  // const handleSearch = async () => {
-  //   try {
-  //     const response = await axios.get(
-  //       `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=${currency}`
-  //     );
-  //     setSearchResult(response.data);
-  //   } catch (error) {
-  //     console.error('Error fetching data:', error);
-  //   }
-  // };
+    const handleSearch = async () => {
+      try {
+        setLoading(true);
+        const response = await axios.get(
+          `https://api.coingecko.com/api/v3/search?query=${currency}`
+        );
+        setSearchResult(response.data);
+        setLoading(false);
+      } catch (error) {
+        console.log('error', error);
+        setLoading(false);
+      }
+    };
 
-  const handleSearch = async () => {
-    try {
-      setLoading(true);
-      const response = await axios.get(
-        `https://api.coingecko.com/api/v3/search?query=${currency}`
-      );
-      setSearchResult(response.data);
-      setLoading(false);
-    } catch (error) {
-      console.log('error', error);
-      setLoading(false);
-    }
-  };
+    const debounceSearch = () => {
+      clearTimeout(timer);
+      timer = setTimeout(() => {
+        handleSearch();
+      }, 300);
+    };
 
-  const handleChange = (e: any) => {
+    debounceSearch();
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [currency]);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setCurrency(e.target.value);
-    handleSearch();
   };
 
   return (
     <div className={`${openSearch ? 'drop-shadow' : ''} relative w-full`}>
       <div
         className={`${
-          openSearch ? 'rounded-t ' : 'drop-shadow rounded'
+          openSearch ? 'rounded-t' : 'drop-shadow rounded'
         } flex items-center gap-2 bg-white px-2 md:px-6 py-2`}
         onClick={() => setOpenSearch(true)}
       >
         <SearchLineIcon />
         <input
           type="text"
-          placeholder={`${currencyName ? currencyName : 'Search by coin'} `}
-          className={` w-full focus:outline-none placeholder:font-semibold ${
+          placeholder={`${currencyName ? currencyName : 'Search by coin'}`}
+          className={`w-full focus:outline-none placeholder:font-semibold ${
             currencyName
               ? 'placeholder:text-black font-semibold'
               : 'placeholder:text-gray-1'
-          } `}
-          value={currencyName}
+          }`}
+          value={currency}
           onChange={handleChange}
         />
       </div>
       {openSearch && (
         <div className="relative">
           <div className="absolute rounded-b w-full bg-white">
-            <div className=" border-b pt-4 px-4 text-gray-400">
+            <div className="border-b pt-4 px-4 text-gray-400">
               {currency.length <= 0 ? 'Trending Search' : 'Cryptocurrencies'}
             </div>
             {currency.length <= 0 && (
               <div>
-                {coinsTrending.map(
-                  ({
-                    item: { id, small, name, symbol, market_cap_rank },
-                  }: any) => (
-                    <button
-                      className="my-2 w-full rounded flex px-4 py-2 justify-between hover:bg-gray-100"
-                      key={name}
-                      onClick={() => {
-                        dispatch(coinValue(id));
-                        setOpenSearch(!openSearch);
-                        setCurrencyName(name + ' (' + symbol + ') ');
-                      }}
-                    >
-                      <div className="text-lg flex items-center gap-2 cursor-pointer">
-                        <img src={small} width="20" height="20" />
-                        {name} ({symbol})
-                      </div>
-                      <div
-                        className={`text-gray-200 ${market_cap_rank ? '' : ''}`}
+                {loading ? (
+                  <div className="py-2 px-4 text-gray-500">Loading...</div>
+                ) : (
+                  coinsTrending.map(
+                    ({
+                      item: { id, small, name, symbol, market_cap_rank },
+                    }: any) => (
+                      <button
+                        className="my-2 w-full rounded flex px-4 py-2 justify-between hover:bg-gray-100"
+                        key={name}
+                        onClick={() => {
+                          dispatch(coinValue(id));
+                          setOpenSearch(false);
+                          setCurrencyName(`${name} (${symbol})`);
+                        }}
                       >
-                        #{market_cap_rank}
-                      </div>
-                    </button>
+                        <div className="text-lg flex items-center gap-2 cursor-pointer">
+                          <img src={small} width="20" height="20" alt={name} />
+                          {name} ({symbol})
+                        </div>
+                        <div
+                          className={`text-gray-200 ${
+                            market_cap_rank ? '' : ''
+                          }`}
+                        >
+                          #{market_cap_rank}
+                        </div>
+                      </button>
+                    )
                   )
                 )}
               </div>
             )}
             {currency.length > 0 && (
               <div className="scroll h-96 overflow-y-scroll">
-                {searchResult?.coins?.map((item: any) => (
-                  <button
-                    className=" my-2 w-full rounded flex px-4 py-2 justify-between hover:bg-gray-100"
-                    key={item.name}
-                    onClick={() => {
-                      dispatch(coinValue(item.id));
-                      setOpenSearch(!openSearch);
-                      setCurrencyName(item.name + ' (' + item.symbol + ') ');
-                    }}
-                  >
-                    <div className="text-lg flex items-center gap-2 cursor-pointer">
-                      <img src={item.large} width="20" height="20" />
-                      {item.name} ({item.symbol})
-                    </div>
-                    {item.market_cap_rank && (
-                      <div className="text-gray-200">
-                        #{item.market_cap_rank}
+                {loading ? (
+                  <div className="py-2 px-4 text-gray-500">Loading...</div>
+                ) : (
+                  searchResult?.coins?.map((item: any) => (
+                    <button
+                      className="my-2 w-full rounded flex px-4 py-2 justify-between hover:bg-gray-100"
+                      key={item.name}
+                      onClick={() => {
+                        dispatch(coinValue(item.id));
+                        setOpenSearch(false);
+                        setCurrencyName(`${item.name} (${item.symbol})`);
+                      }}
+                    >
+                      <div className="text-lg flex items-center gap-2 cursor-pointer">
+                        <img
+                          src={item.large}
+                          width="20"
+                          height="20"
+                          alt={item.name}
+                        />
+                        {item.name} ({item.symbol})
                       </div>
-                    )}
-                  </button>
-                ))}
+                      {item.market_cap_rank && (
+                        <div className="text-gray-200">
+                          #{item.market_cap_rank}
+                        </div>
+                      )}
+                    </button>
+                  ))
+                )}
               </div>
             )}
           </div>
           <button
             className="absolute right-2 -top-8"
-            onClick={() => setOpenSearch(!openSearch)}
+            onClick={() => setOpenSearch(false)}
           >
             X
           </button>
